b798c32717d18a806e6c309095054f3257948188
DESKTOP-PH7CH28\Selin <kocaerselin1@gmail.com>
2025-03-14 15:02:08 +0300

    Contact section completed using emailjs

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,31 @@
 import styles from "./ContactStyles.module.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm("service_jdzk896", "template_vgyzcni", form.current, {
+                publicKey: "Wtr16b4GPvRaLwEnC",
+            })
+            .then(
+                () => {
+                    setMessage("✅ Email sent successfully!");
+                    form.current.reset();
+                },
+                (error) => {
+                    setMessage("❌ Failed to send email: " + error.text);
+                }
+            );
+    };
     return (
         <section id="contact" className={styles.container}>
             <h1 className="sectionTitle">Contact</h1>
-            <form action="">
+            <form ref={form} onSubmit={sendEmail} action="">
                 <div className="formGroup">
                     <label htmlFor="name" hidden>
                         Name
@@ -15,7 +36,7 @@ function Contact() {
                     <label htmlFor="email" hidden>
                         Email
                     </label>
-                    <input type="text" name="email" id="email" placeholder="Email" required />
+                    <input type="email" name="email" id="email" placeholder="Email" required />
                 </div>
                 <div className="formGroup">
                     <label htmlFor="message" hidden>
